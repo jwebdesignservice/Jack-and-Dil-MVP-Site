25d965ddaa3cc03565a4235557bc5b5f347f6fc8
Jack949 <jwebdesign.service@gmail.com>
2026-03-24 17:19:15 +0000

    fix: tech marquee padding 2rem top/bottom

--- components/sections/Marquee.tsx
+++ components/sections/Marquee.tsx
@@ -2,7 +2,7 @@ import MarqueeTicker from '@/components/ui/MarqueeTicker'
 
 export default function Marquee() {
   return (
-    <section className="py-5 bg-[#050505] border-y border-[rgba(249,115,22,0.1)] overflow-hidden relative">
+    <section className="py-8 bg-[#050505] border-y border-[rgba(249,115,22,0.1)] overflow-hidden relative">
       {/* Constrain to content width */}
       <div className="container mx-auto px-6 relative">
         <div className="absolute left-6 top-0 bottom-0 w-16 bg-gradient-to-r from-[#050505] to-transparent z-10 pointer-events-none" />
